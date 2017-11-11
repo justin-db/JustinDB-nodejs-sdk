e3bb3d042546d67808727dec282fd84b95c1afdf
speedcom <mateusz.maciaszekhpc@gmail.com>
2017-11-11 11:53:42 +0100

    basic sceleton of justindb client with exposed get, put async functions

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 'use strict';
 
-function Client(options) {
+var Promise = require('es6-promise').Promise;
 
+function Client(options) { };
+
+Client.prototype.get = async function() {
+  return new Promise(function (resolve, reject) {
+    resolve("GET data from justindb");
+  });
 };
 
-
-Client.prototype.printMsg = function() {
-  console.log("Hello from justindb!!!");
+Client.prototype.put = async function() {
+  return new Promise(function (resolve, reject) {
+    resolve("PUT data to justindb");
+  });
 };
 
 module.exports = Client;
